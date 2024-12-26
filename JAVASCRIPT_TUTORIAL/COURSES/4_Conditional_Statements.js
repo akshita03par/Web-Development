04948747b5429a79ffc0be39ff1c89e1245fc3c0
akshita03par <akshitaparmar2023@gmail.com>
2024-12-26 15:44:53 +0530

    alert and prompt

--- JAVASCRIPT_TUTORIAL/COURSES/4_Conditional_Statements.js
+++ JAVASCRIPT_TUTORIAL/COURSES/4_Conditional_Statements.js
@@ -119,4 +119,35 @@ switch(color2){
             
 }
 
+// day 
+ 
+let day = 5;
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tueday");
+        break;
+    case 3:
+        console.log("Wednesdat");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default :
+        console.log("Wrong day");
+    
+}
+
 
