02d183b59b2db32e4e870a0214a86b8af2776f61
akshita03par <akshitaparmar2023@gmail.com>
2024-12-26 14:27:08 +0530

    new

--- JAVASCRIPT_TUTORIAL/COURSES/4_Conditional_Statements.js
+++ JAVASCRIPT_TUTORIAL/COURSES/4_Conditional_Statements.js
@@ -1,2 +1,91 @@
-// if 
+// if ----------------------------------------
 
+console.log("before my if statement");
+let age=21;
+if(age>=18){
+    console.log("you can vote");
+}
+if(age<18){
+    console.log("you cannot vote");
+}
+console.log("after my if statement");
+
+// create a traffic light system that shows what to do based on color 
+
+let color = "green";
+if(color==="red"){
+    console.log("Stop! light color is red ");
+}
+if(color==="yellow"){
+    console.log("Slow Down ! light color is yellow");
+}
+if(color==="green"){
+    console.log("Go ! light color is green ");
+}
+
+// else if -----------------------------------------------
+
+let age2 = 15 ;
+if(age2 >=18){
+    console.log("you can vote");
+}
+else if(age2<18) {
+   console.log("you cannot vote");
+}
+
+// grade system 
+
+let marks= 22;
+if(marks>=90){
+    console.log("grade is A+");
+} else if(marks>=80){
+    console.log("grade is A");
+} else if(marks>=70){
+    console.log("grade is B+");
+} else if(marks>=60){
+    console.log("grade is B");
+} else if(marks>=50){
+    console.log("grade is C+");
+} else if(marks>=35){
+    console.log("grade is C");
+} else if(marks<35){
+    console.log("F");
+}
+
+//else-----------------------------------------
+
+let age3=18;
+if(age3>=18){
+    console.log("you can vote");
+}else{
+    console.log("you cannot vote ")
+}
+
+
+// create a system to calculate popcorn prices based on the size customer asked for :
+
+let size = 'S';
+if(size==='XL'){
+    console.log("If size is XL then price is Rs.250");
+}else if(size==='L'){
+    console.log("If size is L then price is Rs.200");
+}else if(size==='M'){
+    console.log("If size is M then price is Rs.100");
+}else {
+    console.log(" price is Rs.50");
+}
+
+// nested if-else----------------------------------
+
+let marks2 = 75;
+
+if(marks2>=33){
+    console.log("Pass");
+    if(marks2>=80){
+        console.log("Grade : O");
+    }else{
+        console.log("Grade : A");
+    }
+}else{
+    console.log("Better luck next time");
+}
