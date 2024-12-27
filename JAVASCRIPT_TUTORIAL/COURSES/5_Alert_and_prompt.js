c005e7a3c6b4cbd8df0a1c6b435736bfe2eaab77
akshita03par <akshitaparmar2023@gmail.com>
2024-12-27 10:24:38 +0530

    practice

---
+++ JAVASCRIPT_TUTORIAL/COURSES/5_Alert_and_prompt.js
@@ -0,0 +1,26 @@
+// alert
+alert("Something is wrong!");
+
+// console.error
+console.log("this is a single log");
+console.error("this is an error message");
+
+//console.warn
+console.warn("this is warning message");
+
+//prompt------
+
+// prompt("please Enter your name :");
+
+// let firstName = prompt("please Enter your name!");
+// console.log(firstName);
+
+let firstName = prompt("please Enter your first name!");
+let lastName = prompt("please Enter your last name!");
+
+let msg = "Welcome " + firstName + " " + lastName +" ! ";
+alert(msg);
+
+
+
+
