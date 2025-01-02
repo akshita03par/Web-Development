bb9e13c9d9b14cfce712d1d65b30993a072f3582
akshita03par <akshitaparmar2023@gmail.com>
2025-01-02 19:00:43 +0530

    all types of loops

--- JAVASCRIPT_TUTORIAL/COURSES/10.loops.js
+++ JAVASCRIPT_TUTORIAL/COURSES/10.loops.js
@@ -49,9 +49,99 @@ for(let i=1; i<=3; i++){
 // WHILE LOOP 
 
 console.log("while loop:")
-let i =1;
-while(i <=5){
+// let i =1;
+// while(i <=5){
+//     console.log(i);
+//     i++;
+// }
+
+console.log(" backward while loop:")
+let i =5;
+while(i >=1){
     console.log(i);
-    i++;
+    i--;
+}
+
+
+// favourite movie game 
+
+let favMovie = "avatar" ;
+
+let guess = prompt(" Guess my favourite movie!");
+
+while((guess!=favMovie) ){
+    if((guess== "quit")){
+        console.log("you quit!");
+        break;
+    }
+    guess = prompt("Wrong guess.Please try again");
+}
+
+if(guess==favMovie){
+    console.log("congrats!!!");
+}
+
+// BREAK
+
+let j=1;
+while(j<=5){
+    if(j==3){
+        break;
+    }
+    console.log(j); // 1,2
+    j++;
+}
+
+console.log("we used break at 3");
+
+
+// LOOPS WITH ARRAY 
+
+ let fruits = ["mango","apple","banana","litchi","orange"];
+fruits.push("pineapple");
+ for(let i=0;i<fruits.length;i++){
+    console.log(i,fruits[i]);
+ }
+
+
+//NESTED LOOP  WITH ARRAY 
+
+let heroes = [["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+
+for(let i=0;i<heroes.length;i++){
+    console.log(i,heroes[i],heroes[i].length);
+    for(let j=0; j<heroes[i].length;j++){
+        console.log(`j=${j},${heroes[i][j]}`);
+    }
+}
+
+let student =[["Akshita",95],["shradha",96],["karan",100]];
+
+for(let i=0; i<student.length;i++){
+    console.log(`info of student #${i+1}`);
+    for(let j=0; j<student[i].length; j++){
+        console.log(student[i][j]);
+    }
+}
+
+// for of loop 
+
+let Fruits = ["mango","apple","banana","litchi","orange"];
+for(fruit of Fruits){
+    console.log(fruit);
+}
+
+for(char of "AkshitaParmar"){
+    console.log(char);
+}
+
+// Nested for of loop
+
+let Heroes = [["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+
+for(list of Heroes){
+    for(hero of list){
+        console.log(hero);
+    }
 }
 
