cc3c5b5a26a7b1749564c3529e4b874231aeee8c
akshita03par <akshitaparmar2023@gmail.com>
2025-01-03 23:54:30 +0530

    get values from object

--- JAVASCRIPT_TUTORIAL/COURSES/13.object.js
+++ JAVASCRIPT_TUTORIAL/COURSES/13.object.js
@@ -7,3 +7,29 @@ const student = {
 };
 
 console.log(student);
+
+// THREAD/TWITTER POST---------
+
+// CREATE an object for the properties of thread/twitter post which includes 
+//username , content , likes , reposts , tags 
+
+const post = {
+    username: "@akshitaparmar",
+    content : "this is my #firstpost",
+    likes:150,
+    reposts :5,
+    tags :["@akshita","@likes","@follow"]
+};
+
+console.log(post);
+
+// GET VALUES 
+
+console.log(post.username);
+console.log(post["content"]);
+console.log(post.tags);
+
+let prop = "reposts";
+
+console.log(post[prop]); //5
+
