88d8f3dd7c6f714510762e35cb8de3a13e829feb
akshita03par <akshitaparmar2023@gmail.com>
2025-01-05 14:58:48 +0530

    object practice

---
+++ JAVASCRIPT_TUTORIAL/COURSES/15.object_practice.js
@@ -0,0 +1,29 @@
+
+// 1 dice roll between 1 to 6 
+let dice = Math.floor(Math.random()*6) +1 ;
+console.log(dice);
+
+
+//2 car : name , model , color 
+
+const car = {
+    name : "Maruti Suzuki",
+    model : "Maruti Suzuki Dzire",
+    color : "white"
+};
+console.log(car.name);
+
+//3 
+
+const person = {
+    name : "Akshita",
+    age : 21,
+    city: "Indore"
+};
+
+console.log(person);
+console.log(person.city="New York");
+console.log(person);
+console.log(person.country="India");
+console.log(person);
+
