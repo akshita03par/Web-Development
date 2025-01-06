6552fc85719e6d155817baa3b6667a3353d304f5
akshita03par <akshitaparmar2023@gmail.com>
2025-01-06 19:15:31 +0530

    function practice

--- JAVASCRIPT_TUTORIAL/COURSES/16.function.js
+++ JAVASCRIPT_TUTORIAL/COURSES/16.function.js
@@ -94,3 +94,133 @@ avg(56,45,89);
 
 // multiplication table of number 
 
+function printTable(n){
+     console.log("Multiplication table of",n);
+     for(let i = n ; i<=n*10; i+=n){
+        console.log(i);
+     }
+}
+
+printTable(12);
+
+// RETURN 
+
+function sum(a,b){
+    console.log("hello");
+    return a+b;
+    console.log("hello"); // kabhi print nahi hogi
+}
+
+let s = sum(3,4);
+console.log(s);
+
+// console.log(sum(3,4));
+
+// console.log(sum(sum(1,2),3)); //6
+
+//age 
+
+function IsAdult(age){
+    if(age >= 18){
+        return "adult";
+    }else{
+        return "not adult";
+
+    }
+}
+
+console.log(IsAdult(21));
+
+// create a function that return the sum of number from 1 to n.
+
+function getSum(n){
+    let sum =0;
+    for(let i= 1 ;i<=n;i++){
+        sum+=i;
+    }
+
+    return sum ;
+}
+
+console.log(getSum(10));
+
+// create a function that returns the concatenation of all strings in an array.
+
+let str = ["hi","hello","bye","!"];
+
+function Concat(str) {
+    let result ="";
+
+    for(let i=0;i<str.length; i++){
+        result += str[i];
+    }
+
+    return result;
+}
+
+console.log(str);
+console.log(Concat(str));
+
+
+// SCOPE 
+
+let sm= 54; //Global Scope
+
+function calSum(a,b){
+    let sum = a+b; // function scope
+    console.log(sum);
+}
+
+calSum(1,2);
+// console.log(sum); ---> function scope does work give an error
+
+
+//2 block scope 
+ console.log("block scope----");
+ 
+for(let i =1 ;i<=5 ; i++){
+    console.log(i); // block scope
+}
+
+// console.log(i);----> give an error 
+
+//3 lexical scope 
+
+console.log("block scope----");
+function outFunc() {
+    let x = 5;
+    let y = 6;
+
+    function innerFunc(){
+        console.log(x); // lexical scope
+    }
+
+    innerFunc();
+}
+outFunc();
+
+// next practice 
+
+let greet = "hello";  //global scope
+
+function changeGreet(){
+    let greet = "namaste";  //function scope
+    console.log(greet);
+
+    function innerGreet(){
+        console.log(greet); // lexical scope
+    }
+    innerGreet();
+}
+
+console.log(greet);
+changeGreet();
+
+//  FUNCTION EXPRESSIONS 
+
+let sumOfnum = function(a , b){
+    return a+b;
+}
+
+console.log(sumOfnum(3,5)); //8
+
