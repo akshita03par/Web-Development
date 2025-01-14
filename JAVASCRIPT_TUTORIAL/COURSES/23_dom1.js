d28a5bb17972c314c9443c3748e4c15009d39545
akshita03par <akshitaparmar2023@gmail.com>
2025-01-14 18:59:42 +0530

    dom start

---
+++ JAVASCRIPT_TUTORIAL/COURSES/23_dom1.js
@@ -0,0 +1,13 @@
+let smallImages = document.getElementsByClassName("oldImg");
+
+for(let i=0; i<smallImages.length; i++){
+    console.log(smallImages[i].src);
+}
+
+// change image 
+
+// for(let i=0; i<smallImages.length; i++){
+//     smallImages[i].src = "./Images/spider man.jpg";
+//     console.log(`value of image no.${i} is changed`);
+// }
+
