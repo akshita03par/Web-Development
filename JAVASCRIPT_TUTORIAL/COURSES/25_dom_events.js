deff298ffe18065f684de2c63d0cc73579c19ef6
akshita03par <akshitaparmar2023@gmail.com>
2025-01-21 18:01:23 +0530

    such more events

--- JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
+++ JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
@@ -25,4 +25,23 @@ function sayHello() {
     alert("Hello!");
 }
 
-//
+// Event Listener ------->
+
+//addEventListener
+
+// btn.addEventListener("click",function() {
+//     console.log("button click");
+// });
+
+for(btn of btns){
+    btn.addEventListener("click",sayHello);
+    btn.addEventListener("click",sayName);
+    btn.addEventListener("dbclick", function (){
+        console.log("you double click");
+    });
+}
+
+function sayName() {
+    console.log("Akshita parmar");
+}
+
