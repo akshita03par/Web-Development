683225ef8adc45cbcf166fa707f37165ef6a4be0
akshita03par <akshitaparmar2023@gmail.com>
2025-01-21 17:40:55 +0530

    click events

--- JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
+++ JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
@@ -1,2 +1,28 @@
 // Dom events ------------------>
 
+// onclick 
+
+let btn = document.querySelector("button");
+console.dir(btn);
+
+btn.onclick = function () {
+    console.log("button was clicked");
+};
+
+// for all button--->
+
+let btns = document.querySelectorAll("button");
+
+for(btn of btns){
+    btn.onclick = sayHello;
+    btn.onmouseenter = function () {
+        console.log("you entered a button");
+
+    };
+}
+
+function sayHello() {
+    alert("Hello!");
+}
+
+//
