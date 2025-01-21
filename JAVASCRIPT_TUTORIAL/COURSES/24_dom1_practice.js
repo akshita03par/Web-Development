933c1ce9ec759de4fd281388bc2749b2a0eebfc0
akshita03par <akshitaparmar2023@gmail.com>
2025-01-21 12:24:07 +0530

    query selector

--- JAVASCRIPT_TUTORIAL/COURSES/24_dom1_practice.js
+++ JAVASCRIPT_TUTORIAL/COURSES/24_dom1_practice.js
@@ -39,3 +39,31 @@ p.innerText = "ME TOO!";
 // document.querySelector("div").append(p);
 div.append(p);
 
+
+//Qs1. Create a new input and button element on the page using JavaScript only. Set the text of button to “Click me”
+
+let button = document.createElement("button");
+let input = document.createElement("input");
+
+button.innerText = "Click me !";
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+//Qs2. Add following attributes to the element :
+//- Change placeholder value of input to “username”
+//- Change the id of button to “btn”
+
+button.setAttribute("placeholder","username");
+button.setAttribute("id","btn");
+
+//Qs3. Access the btn using the querySelector and button id. Change the button background color to blue and text color to white
+
+let btn = document.querySelector("#btn");
+button.classList.add("btnstyle");
+
+//
+
+
+
+
