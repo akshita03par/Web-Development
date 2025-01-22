5593fffc4bb8ae86399a9d7f36555d0f44713d9c
akshita03par <akshitaparmar2023@gmail.com>
2025-01-22 19:51:03 +0530

    this in event listener

--- JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
+++ JAVASCRIPT_TUTORIAL/COURSES/25_dom_events.js
@@ -33,15 +33,52 @@ function sayHello() {
 //     console.log("button click");
 // });
 
-for(btn of btns){
-    btn.addEventListener("click",sayHello);
-    btn.addEventListener("click",sayName);
-    btn.addEventListener("dbclick", function (){
-        console.log("you double click");
-    });
-}
+// for(btn of btns){
+//     btn.addEventListener("click",sayHello);
+//     btn.addEventListener("click",sayName);
+//     btn.addEventListener("dbclick", function (){
+//         console.log("you double click");
+//     });
+// }
 
 function sayName() {
     console.log("Akshita parmar");
 }
+
+let p = document.querySelector("p");
+
+p.addEventListener("click",function () {
+   console.log("parah was clicked");
+});
+
+let box = document.querySelector(".box");
+
+box.addEventListener("mouseenter",function(){
+     console.log("mouse inside box");
+});
+
+
+// this in event listeners ---------
+
+
+let btn2 = document.querySelector("button");
+
+// btn2.addEventListener("click",function(){
+//     console.log(this);
+//     this.style.backgroundColor = "blue";
+// });
+
+let P = document.querySelector("p");
+let H1 = document.querySelector("h1");
+let H3 = document.querySelector("h3");
+
+function changeColor() {
+    console.log(this.innerText);
+    this.style.color = "red";
+}
+
+btn2.addEventListener("click",changeColor);
+P.addEventListener("click",changeColor);
+H1.addEventListener("click",changeColor);
+H3.addEventListener("click",changeColor);
 
