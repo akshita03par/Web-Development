aa5a17bc8f51f2e3395e3731baaa1f6ce3e138a5
akshita03par <akshitaparmar2023@gmail.com>
2025-01-24 21:43:43 +0530

    promises completed

--- JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
+++ JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
@@ -50,20 +50,55 @@ console.log("hello..."); // 1
 
 let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-  setTimeout(() => {
-    h1.style.color = color;
-    if (nextColorChange) nextColorChange();
-  }, delay);
-}
+// function changeColor(color, delay, nextColorChange) {
+//   setTimeout(() => {
+//     h1.style.color = color;
+//     if (nextColorChange) nextColorChange();
+//   }, delay);
+// }
 
-changeColor("purple", 1000, () => {
-  changeColor("Orange", 1000, () => {
-    changeColor("green", 1000, () => {
-      changeColor("blue", 1000);
-    });
-  });
-});
+// changeColor("purple", 1000, () => {
+//   changeColor("Orange", 1000, () => {
+//     changeColor("green", 1000, () => {
+//       changeColor("blue", 1000);
+//     });
+//   });
+// });
+
+console.log("---------------------------------");
+
+// promises apply ----->
+
+
+function changeColor(color, delay) {
+   return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed!");
+          }, delay);
+    });  
+  }
+  
+//  let requestPromise = changeColor("red",1000);
+//  console.log(requestPromise);
+
+changeColor("red",1000)
+.then(()=>{
+    console.log("red color was completed");
+    return changeColor("orange",1000);
+})
+.then(()=>{
+    console.log("orange color was completed");
+    return changeColor("green",2000);
+})
+.then(()=>{
+    console.log("green color was completed");
+    return changeColor("blue",3000);
+})
+.then(()=>{
+    console.log("blue color was completed");
+})
+
 
 // setTimeout(()=>{
 //     h1.style.color = "purple";
@@ -73,7 +108,8 @@ changeColor("purple", 1000, () => {
 //     h1.style.color = "Orange";
 // },2000);
 
-// Promises----------->
+
+// Promises-------------------------------------------------------------->
 
 // function savetoDb(data, success, failure) {
 //   let internetSpeed = Math.floor(Math.random() * 10) + 1;
@@ -113,24 +149,22 @@ changeColor("purple", 1000, () => {
 //   }
 // );
 
-
 // refactoring with promise ------>
 
 function savetoDb(data) {
-    return new Promise((resolve,reject)=>{
-        let internetSpeed = Math.floor(Math.random() * 10) + 1;
-        if(internetSpeed > 4){
-            resolve("success: data was saved");
-        }else{
-            reject("failure: weak connection");
-        }
-    });
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("success: data was saved");
+    } else {
+      reject("failure: weak connection");
+    }
+  });
 }
 
 // savetoDb("akshita parmar");
 
-
-// then() & catch()----->
+// then() & catch()------------------------------------------->
 
 // let request = savetoDb("Akshita Parmar"); // req = promise object
 
@@ -143,11 +177,52 @@ function savetoDb(data) {
 //     console.log(request);
 // });
 
+// savetoDb("Akshita Parmar")
+//  .then(() =>{
+//     console.log("promise was resolved");
+// })
+// .catch(()=>{
+//     console.log("promise was rejected");
+// });
+
+// chaining promises --> improved version --------------------->
+
 savetoDb("Akshita Parmar")
- .then(() =>{
-    console.log("promise was resolved");
-})
-.catch(()=>{
+  .then(() => {
+    console.log("data1 saved");
+    return savetoDb("hello world");
+  })
+  .then(() => {
+    console.log("data2 saved");
+    return savetoDb("Akshita");
+  })
+  .then(() => {
+    console.log("data3 saved");
+  })
+  .catch(() => {
     console.log("promise was rejected");
-});
+  });
+
+// promises are rejectes and resolved eith some data (valid resilts or errors)---->
+
+savetoDb("Akshita Parmar")
+  .then((result) => {
+    console.log("data1 saved");
+    console.log("result of promise:",result);
+    return savetoDb("hello world");
+  })
+  .then((result) => {
+    console.log("data2 saved");
+    console.log("result of promise:",result);
+    return savetoDb("Akshita");
+  })
+  .then((result) => {
+    console.log("data3 saved");
+    console.log("result of promise:",result);
+  })
+  .catch((error) => {
+    console.log("promise was rejected");
+    console.log("error of promise:",error);
+  });
+
 
