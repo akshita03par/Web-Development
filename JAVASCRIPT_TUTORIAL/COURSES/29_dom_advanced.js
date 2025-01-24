a76a096ede7ee5f08ecf3de99985b063673e31e9
akshita03par <akshitaparmar2023@gmail.com>
2025-01-24 21:05:27 +0530

    then and catch

--- JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
+++ JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
@@ -75,47 +75,79 @@ changeColor("purple", 1000, () => {
 
 // Promises----------->
 
-function savetoDb(data, success, failure) {
-  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+// function savetoDb(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1;
 
-  if (internetSpeed > 4) {
-    success();
-  } else {
-    failure();
-  }
-}
+//   if (internetSpeed > 4) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
 
-savetoDb(
-  "Akshita Parmar",
-  () => {
-    console.log("success: your data was saved ");
-    savetoDb(
-      "hello World",
-      () => {
-        console.log("sucess2: data2 saved");
-        savetoDb(
-          "Akshita",
-          () => {
-            console.log("sucess3: data3 saved");
-          },
-          () => {
-            console.log("failure3: weak connection");
-          }
-        );
-      },
-      () => {
-        console.log("failure2: weak connection");
-      }
-    );
-  },
-  () => {
-    console.log("failure: weak connection,data not saved");
-  }
-);
+// savetoDb(
+//   "Akshita Parmar",
+//   () => {
+//     console.log("success: your data was saved ");
+//     savetoDb(
+//       "hello World",
+//       () => {
+//         console.log("sucess2: data2 saved");
+//         savetoDb(
+//           "Akshita",
+//           () => {
+//             console.log("sucess3: data3 saved");
+//           },
+//           () => {
+//             console.log("failure3: weak connection");
+//           }
+//         );
+//       },
+//       () => {
+//         console.log("failure2: weak connection");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("failure: weak connection,data not saved");
+//   }
+// );
 
 
 // refactoring with promise ------>
 
+function savetoDb(data) {
+    return new Promise((resolve,reject)=>{
+        let internetSpeed = Math.floor(Math.random() * 10) + 1;
+        if(internetSpeed > 4){
+            resolve("success: data was saved");
+        }else{
+            reject("failure: weak connection");
+        }
+    });
+}
+
+// savetoDb("akshita parmar");
 
 
+// then() & catch()----->
+
+// let request = savetoDb("Akshita Parmar"); // req = promise object
+
+// request.then(() =>{
+//     console.log("promise was resolved");
+//     console.log(request);
+// })
+// .catch(()=>{
+//     console.log("promise was rejected");
+//     console.log(request);
+// });
+
+savetoDb("Akshita Parmar")
+ .then(() =>{
+    console.log("promise was resolved");
+})
+.catch(()=>{
+    console.log("promise was rejected");
+});
 
