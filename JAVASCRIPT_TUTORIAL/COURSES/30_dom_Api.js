29b74c1251c8ba2b93a970571c0d5b6d9b523ee6
akshita03par <akshitaparmar2023@gmail.com>
2025-01-25 18:05:49 +0530

    api using fetch

--- JAVASCRIPT_TUTORIAL/COURSES/30_dom_Api.js
+++ JAVASCRIPT_TUTORIAL/COURSES/30_dom_Api.js
@@ -1,22 +1,44 @@
 //json data ---> js object
 
-let jsonRes = 
- '{"fact":"On average, cats spend 2\/3 of every day sleeping. That means a nine-year-old cat has been awake for only three years of its life.","length":129}';
+// let jsonRes = 
+//  '{"fact":"On average, cats spend 2\/3 of every day sleeping. That means a nine-year-old cat has been awake for only three years of its life.","length":129}';
 
-let validRes = JSON.parse(jsonRes);
-console.groupCollapsed(validRes);
+// let validRes = JSON.parse(jsonRes);
+// console.groupCollapsed(validRes);
 
 // js object------> json data 
 
-let student = {
-    name: "Akshita",
-    age:21,
-    marks:95
-};
+// let student = {
+//     name: "Akshita",
+//     age:21,
+//     marks:95
+// };
 
-let jsJson = JSON.stringify(student);
-console.log(jsJson);
+// let jsJson = JSON.stringify(student);
+// console.log(jsJson);
 
-// testing API requests ----->
 
+// api request -------------------------->
+// using fetch 
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+.then((response)=>{
+  console.log(response);
+  return response.json();
+})
+.then((data1)=>{
+    console.log("data1 : ",data1);
+    return fetch(url);
+})
+.then((response)=>{
+    return response.json();
+})
+.then((data2)=>{
+    console.log("data2 : ",data2);
+})
+.catch((e)=>{
+    console.log("ERROR - ",e);
+});
 
