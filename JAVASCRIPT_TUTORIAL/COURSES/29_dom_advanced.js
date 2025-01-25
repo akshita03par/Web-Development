e130671b60747b957951773777765146395f89bf
akshita03par <akshitaparmar2023@gmail.com>
2025-01-25 12:19:06 +0530

    error handling in await

--- JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
+++ JAVASCRIPT_TUTORIAL/COURSES/29_dom_advanced.js
@@ -250,7 +250,7 @@ function getNum() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let num = Math.floor(Math.random() * 10) + 1;
-      console.log("num :",num);
+      console.log("num :", num);
       resolve();
     }, 1000);
   });
@@ -260,37 +260,42 @@ async function Demo() {
   await getNum();
   await getNum();
   await getNum();
- 
 }
 
- Demo();
+Demo();
 
+// change color with async function ---->
 
- // change color with async function ---->
+h2 = document.querySelector("h2");
 
- h2 = document.querySelector("h2");
-
- function ChangeColor(color,delay){
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-      
-
+function ChangeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
       h2.style.color = color;
       console.log(`color changed to : ${color}!`);
       resolve("color chnaged!");
-    },delay);
+    }, delay);
   });
- }
+}
 
- async function Demochange(){
-   await ChangeColor("brown",1000);
-   await ChangeColor("pink",1000);
-   await ChangeColor("violet",1000);
-   await ChangeColor("purple",1000);
+async function Demochange() {
+  try {
+    await ChangeColor("brown", 1000);
+    await ChangeColor("pink", 1000);
+    await ChangeColor("violet", 1000);
+    await ChangeColor("purple", 1000);
+  } catch (error) {
+    console.log("error caught");
+    console.log(error);
+  }
 
+  let a = 5;
+  console.log(a);
+  console.log("new number = ", a + 3);
+}
 
- }
-
- Demochange();
-
-
+Demochange();
