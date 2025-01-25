2368be19244639abc919f69c2c55db03c7bdd8b3
akshita03par <akshitaparmar2023@gmail.com>
2025-01-25 18:14:34 +0530

    using fetch with async and await

--- JAVASCRIPT_TUTORIAL/COURSES/30_dom_Api.js
+++ JAVASCRIPT_TUTORIAL/COURSES/30_dom_Api.js
@@ -1,12 +1,12 @@
 //json data ---> js object
 
-// let jsonRes = 
+// let jsonRes =
 //  '{"fact":"On average, cats spend 2\/3 of every day sleeping. That means a nine-year-old cat has been awake for only three years of its life.","length":129}';
 
 // let validRes = JSON.parse(jsonRes);
 // console.groupCollapsed(validRes);
 
-// js object------> json data 
+// js object------> json data
 
 // let student = {
 //     name: "Akshita",
@@ -17,28 +17,45 @@
 // let jsJson = JSON.stringify(student);
 // console.log(jsJson);
 
-
 // api request -------------------------->
-// using fetch 
+// using fetch
 
 let url = "https://catfact.ninja/fact";
 
 fetch(url)
-.then((response)=>{
-  console.log(response);
-  return response.json();
-})
-.then((data1)=>{
-    console.log("data1 : ",data1);
-    return fetch(url);
-})
-.then((response)=>{
+  .then((response) => {
+    console.log(response);
     return response.json();
-})
-.then((data2)=>{
-    console.log("data2 : ",data2);
-})
-.catch((e)=>{
-    console.log("ERROR - ",e);
-});
+  })
+  .then((data1) => {
+    console.log("data1 : ", data1);
+    return fetch(url);
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data2) => {
+    console.log("data2 : ", data2);
+  })
+  .catch((e) => {
+    console.log("ERROR - ", e);
+  });
+
+// using fetch with async and await ----->
+
+async function getFacts() {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2);
+  } catch (e) {
+    console.log("error - ", e);
+  }
+}
+
+getFacts();
 
