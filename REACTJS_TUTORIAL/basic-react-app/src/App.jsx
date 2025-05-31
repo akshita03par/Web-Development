b6a809f63535d251d3b27f8f2b95c04e2ee305ef
akshita03par <akshitaparmar2023@gmail.com>
2025-05-31 11:48:15 +0530

    1st component

--- REACTJS_TUTORIAL/basic-react-app/src/App.jsx
+++ REACTJS_TUTORIAL/basic-react-app/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+//import title component
+import Title from "./Title.jsx";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// // first Component
+
+// function Title() {
+//   return <h1>I am the Title!</h1>;
+// }
+
+function Description() {
+  return <h1>This is my app component</h1>;
 }
 
-export default App
+function App() {
+  // return <h1>Hello World!</h1>;
+
+  // rendering a component
+  return (
+    <div>
+      <Description/>
+      <p>inside app component we have:</p>
+      <Title />
+      {/* <Title/> */}
+    </div>
+  );
+}
+export default App;
