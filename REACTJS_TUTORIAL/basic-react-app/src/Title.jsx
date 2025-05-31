24dcc602e3ae0e802f81b98a4a88b4471829ac9d
akshita03par <akshitaparmar2023@gmail.com>
2025-05-31 11:48:15 +0530

    1st component

---
+++ REACTJS_TUTORIAL/basic-react-app/src/Title.jsx
@@ -0,0 +1,11 @@
+// first Component
+
+function Title() {
+  return <h1>I am the Title!</h1>;
+}
+
+//export title component
+
+export default Title; //---> export sigle value
+
+// export {Title}; ----> use for multiple export 
