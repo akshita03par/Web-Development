60993dfe8e36ad3e890778bb9031435e977ad0fe
akshita03par <akshitaparmar2023@gmail.com>
2025-05-31 17:18:51 +0530

    props in react

--- REACTJS_TUTORIAL/basic-react-app/src/Product.jsx
+++ REACTJS_TUTORIAL/basic-react-app/src/Product.jsx
@@ -1,15 +1,21 @@
-import "./Product.css" ;
+import "./Product.css";
 
-
-// Structuring component
-function Product(){
-    return(
-        <div className="Product">
-            <h3>Product Title</h3>
-            <h5>Product Description</h5>
-        </div>
-    );
+function Product({ title, price, features }) {
+  return (
+    <div className="Product">
+      <h3>{title}</h3>
+      <h5>Price : {price}</h5>
+      {features ? (
+        <ul>
+          {features.map((feature, i) => (
+            <li key={i}>{feature}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No features listed</p>
+      )}
+    </div>
+  );
 }
 
 export default Product;
-
--- REACTJS_TUTORIAL/basic-react-app/src/ProductTab.jsx
+++ REACTJS_TUTORIAL/basic-react-app/src/ProductTab.jsx
@@ -1,11 +1,22 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  let options = ["hi-tech", "durable", "fast"]; // ----> array to props-1
+
+  // let options = [<li>"hi-tech"</li>,<li> "durable"</li>,<li>"fast"</li>]; -2
+
+  // let options2 = { a: "hi-tech", b: "durable", c: "fast" };// object
+
   return (
     <>
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        title="phone"
+        price={30000}
+        features={options}
+        // features2={options2}
+      />
+      <Product title="laptop" price={80000} />
+      <Product title="pen" price={10} />
     </>
   );
 }
