c44dfe0d61c4c6d189fc7ed9a582c65abe75279a
akshita03par <akshitaparmar2023@gmail.com>
2025-06-04 14:20:56 +0530

    like Button

--- REACTJS_TUTORIAL/COURSE/react-state-class/src/App.jsx
+++ REACTJS_TUTORIAL/COURSE/react-state-class/src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import Counter from "./Counter";
 
+import LikeButton from "./LikeButton";
+
+
 function App() {
   return (
     <>
+
       <h1>States in React</h1>
-      <Counter/>
+      
+      {/* <Counter/> */}
+   
+      <LikeButton/>
+
     </>
   );
 }
 
 export default App;
+
