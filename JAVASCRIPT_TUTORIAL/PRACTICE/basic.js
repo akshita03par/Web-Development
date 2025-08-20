7de1122f3abda6b38adf368b3d05fb8c8c8700f9
akshita03par <akshitaparmar2023@gmail.com>
2025-08-20 11:29:22 +0530

    Number and Math

---
+++ JAVASCRIPT_TUTORIAL/PRACTICE/basic.js
@@ -0,0 +1,62 @@
+"use strict"; // treat all js code as newer version 
+
+console.log("Akshita");
+
+let name = "Akshita Parmar";
+var age = 22;
+
+console.table([name,age]); // output in form of table 
+
+console.log(typeof(age)); //number
+
+//<-------------conversion------------->
+
+let score = "33Abc";
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// number to number --- > easily convert
+// numberAlpha ("33ABC")----> NaN
+
+
+//<----------------comparison--------------->
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+//<------------symbol datatype--------->
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id === anotherId); // false
+
+//----------------Number------------------------>
+
+const balance = new Number(100); //object
+console.log(balance);
+
+console.log(balance.toString().length); // 3 
+
+//fixed
+console.log(balance.toFixed(3)); // 100.000
+
+//precision
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4)); //123.9
+console.log(otherNumber.toPrecision(3)); // 124
+
+//localString 
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString()); //1,000,000
+console.log(hundreds.toLocaleString('en-IN')); //10,00,000
+
+//<---------------MATH--------------->
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
