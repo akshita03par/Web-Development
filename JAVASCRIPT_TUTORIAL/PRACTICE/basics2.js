cb5864a0405b5569a93fe1a6c43bbb8d811b5dec
akshita03par <akshitaparmar2023@gmail.com>
2025-08-20 16:02:56 +0530

    array concepts

--- JAVASCRIPT_TUTORIAL/PRACTICE/basics2.js
+++ JAVASCRIPT_TUTORIAL/PRACTICE/basics2.js
@@ -22,3 +22,28 @@ const newArr = myArr.join();
 console.log(newArr);
 console.log(typeof(newArr));
 
+// spread concept------------->
+
+const marvel_heroes = ['Thor','IronMan','SpiderMan'];
+const dc_heroes = ['SuperMan' , 'Flash' , 'BatMan'];
+
+const allHeores = [...marvel_heroes,...dc_heroes];
+console.log(allHeores);
+
+// flat------>
+
+const anotherArray = [1,2,3,[4,5,6],7,[8,9,[10,11]]];
+console.log(anotherArray);
+const realArray = anotherArray.flat(Infinity);
+console.log(realArray);
+
+//is form -------->
+console.log(Array.from("Akshita"));
+
+// make array --->
+
+let src1 = 100;
+let src2 = 200;
+let src3 = 300;
+
+console.log(Array.of(src1,src2,src3));
